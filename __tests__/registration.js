150117330d8d56f634eae601a7c2341bebf8e14b
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-09-13 20:01:18 -0400

    made test directory, wrote a test for registration endpoint, started writing registration endpoint

---
+++ __tests__/registration.js
@@ -0,0 +1,41 @@
+// want to test:
+
+// statusCode = 400 if no username provided
+// statusCode = 400 if no password
+// statusCode = 201 if success
+// data type = json if success
+// data = newUser info if success
+
+// import supertest, our server, and our db
+const supertest = require("supertest")
+const server = require("../api/server")
+const db = require("../database/dbConfig")
+
+// clear the database before each test runs
+beforeEach(async () => {
+    await db("users").truncate()
+})
+
+// get rid of db connection after all tests are done
+afterAll(async () => {
+    await db.destroy()
+})
+
+// write tests
+describe("registration integration tests", () => {
+    it("POST /api/auth/register", async () => {
+        const res = await supertest(server)
+            .post("/api/auth/register")
+            .send({
+                username: "jenny",
+                password: "abc123"
+            })
+        // statusCode should be 201
+        expect(res.statusCode).toBe(201)
+        // type should be json
+        expect(res.type).toBe("application/json")
+        // data should be the object we sent in, with a hashed password
+        expect(res.body.username).toBe("jenny")
+        expect(res.body.id).toBe(1)
+    })
+})
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
+const bcrypt = require("bcryptjs")
 
-router.post('/register', (req, res) => {
+router.post("/register", async (req, res, next) => {
   // implement registration
-});
+  try {
+    const { username, password } = req.body
+    const
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.post('/login', (req, res) => {
   // implement login
-});
+})
 
 module.exports = router;
